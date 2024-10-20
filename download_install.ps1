5cc429349721b61528868a38bc048397e647f4f0
Y-CMB <cbarry000@gmail.com>
2024-10-20 19:51:07 -0400

    Syntax change to Start-Process

--- download_install.ps1
+++ download_install.ps1
@@ -1,5 +1,6 @@
 Invoke-WebRequest -Uri "https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe" -OutFile "C:/Users/Public/Public Downloads/SteamSetup.exe"
-Start-Process -FilePath "C:/Users/Public/Public Downloads/SteamSetup.exe" -ArgumentList '/S' -Wait
+Start-Process -FilePath "SteamSetup.exe" -WorkingDirectory "C:\Users\Public\Public Downloads" -Wait
 Invoke-WebRequest -Uri "https://origin-a.akamaihd.net/EA-Desktop-Client-Download/installer-releases/EAappInstaller.exe" -OutFile "C:/Users/Public/Public Downloads/EAappInstaller.exe"
-Start-Process -FilePath "C:/Users/Public/Public Downloads/EAappInstaller.exe" -ArgumentList '/S' -Wait
+Start-Process -FilePath "EAappInstaller.exe" -WorkingDirectory "C:\Users\Public\Public Downloads" -Wait
 Invoke-WebRequest -Uri "https://legi.cc/downloads/genpatcher/GenPatcher%202.07f.zip" -OutFile "C:/Users/Public/Public Downloads/GenPatcher.zip"
+Expand-Archive -Path "C:/Users/Public/Public Downloads/GenPatcher.zip" -DestinationPath "C:\Users\Public\Public Downloads"
